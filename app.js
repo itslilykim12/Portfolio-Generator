530b3223090ff9ff084c6e8fd7a097f13b7dd5b9
nlilu <nlilum.kim@gmail.com>
2021-04-17 21:23:59 -0400

    generated an index.html

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const profileDataArgs = process.argv.slice(2); 
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    //This ...
-    for (let i =0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('================');
+const generatePage = require('./src/page-template.js');
 
-    //Is the same as this .... 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const profileDataArgs = process.argv.slice(2);
 
-printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
